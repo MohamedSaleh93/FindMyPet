e09490969e97a7407e284a19deb67315ea2a0552
MohamedSaleh93 <mohamedsaleh.fcis@gmail.com>
2024-10-28 21:30:34 +0100

    Add implementation for pet tracker that return random numbers

--- app/src/main/java/com/mohamedsaleh/findmypet/data/local/PetTrackerScanner.kt
+++ app/src/main/java/com/mohamedsaleh/findmypet/data/local/PetTrackerScanner.kt
@@ -1,10 +1,19 @@
 package com.mohamedsaleh.findmypet.data.local
 
 import com.mohamedsaleh.findmypet.data.local.IPetTrackerScanner
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flow
+import kotlin.random.Random
 
 class PetTrackerScanner: IPetTrackerScanner {
 	override fun scanPetTracker(): Flow<Int> {
-		TODO("Not yet implemented")
+		return flow {
+			while (true) {
+				emit(Random.nextInt(0, 30))
+				delay(2000)
+			}
+		}
 	}
 }
