e679a93021c1e4fb5bea7e9e624c178bfeb2763a
MohamedSaleh93 <mohamedsaleh.fcis@gmail.com>
2024-10-28 19:30:26 +0100

    Align the code packages

---
+++ app/src/main/java/com/mohamedsaleh/findmypet/presentation/PetTrackerActivity.kt
@@ -0,0 +1,24 @@
+package com.mohamedsaleh.findmypet.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.mohamedsaleh.findmypet.ui.theme.FindMyPetTheme
+
+class PetTrackerActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			FindMyPetTheme {
+				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+				}
+			}
+		}
+	}
+}
